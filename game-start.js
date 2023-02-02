7ad87ab6542a0d0bcdd4a92e47c0609e1b9d2ed9
Amoatey <emmanuel.amoatey@amalitech.org>
2023-02-02 10:24:26 +0000

    linked buttons to various interfaces

--- game-start.js
+++ game-start.js
@@ -1,51 +1,63 @@
-const gameBoard = document.getElementbyId("Game-Start");
+const gameStart = document.getElementById('gameStart')
+const cellBoard = document.getElementsByClassName('cellBoard')
+const headDisplay = document.getElementsByClassName('headDisplay')
+const playerTurnDisplay = document.getElementsByClassName('player-turn')
+const cellContainer = document.getElementById ('cellContainer')
 
+// restart modal Display
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay')
+const closeModalBtn = document.querySelector('.btn-close')
 
-const winnerX = false;
-const winnerO = false;
-const cell0 = document.getElementById("0");
-const cell1 = document.getElementById("1");
-const cell2 = document.getElementById("2");
-const cell3 = document.getElementById("3");
-const cell4 = document.getElementById("4");
-const cell5 = document.getElementById("5");
-const cell6 = document.getElementById("6");
-const cell7 = document.getElementById("7");
-const cell8 = document.getElementById("8");
-
-var origBoard = Array.from(Array(9).keys());
-var huPlayer;
-var aiPlayer;
-
-const win_pattern = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6],
-];
-
-function newGameCPU() {
-  gameBoard.style.display = "initial";
-  gameMenu.style.display = "none";
-  if (XradioBtn.checked === true) {
-    huPlayer = "X";
-    aiPlayer = "O";
-    player1Name.innerHTML = "X (You)";
-    player2Name.innerHTML = "0 (CPU)";
-  } else {
-    huPlayer = "O";
-    aiPlayer = "X";
-    player1Name.innerHTML = "X (CPU)";
-    player2Name.innerHTML = "0 (You)";
-  }
-  cpuBtn.setAttribute("data-value", "active");
-  playerBtn.setAttribute("data-value", "");
-
-  cpuTurn();
+const openModal = function () {
+  modal.classList.remove('hidden')
+  overlay.classList.remove('hidden')
 }
+restartBtn.addEventListener('click', openModal)
 
+// close modal 
+ function closeModal() {
+  modal.classList.add('hidden')
+  overlay.classList.add('hidden')
+}
+closeModalBtn.addEventListener('click', closeModal)
+let yesRestartBtn= document
+  .querySelector(".Yes-Restart")
+  .addEventListener("click", () => {
+    window.location.href = "new-game.html";
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+// turn indicator
+function hitCell(cell) {
+  let boxChoice = document.getElementById(cell)
+   img.src = document.createElement('img')
+
+   if (player-turn.getAttribute('data-value') === "X") {
+    img.src = './assets/icon-x.svg';
+    img.setAttribute('class' , 'cellPlayed');
+    cellChoice.appendChild(img);
+    cellChoice.classList.remove('hoverClassX');
+    cellChoice.setAttribute('data-value' , 'X');
+    cellChoice.setAttribute('onclick', '' )
+    turn.setAttribute('data-value', 'O')
+    turn.src = './assets/icon-o-turn.svg';
+   }
+}
 
--- new-game.js
+++ new-game.js
@@ -5,9 +5,88 @@ markX.addEventListener('click', function(){
   // console.log('click');
   markX.checked = true;
   markO.checked = false;
-});
+})
 markO.addEventListener('click', function(){
   markO.checked = true;
   markX.checked = false;
 })
+
+
+
+// cpu vs player button function (single player)
+let cpuBtn = document.getElementById('newGameVSCPU')
+.addEventListener('click', () => {
+  window.location.href = 'game-start.html'
+})
+
+// new game vs player (multi player button)
+let multiPlayerBtn = document.getElementById('newGameVSPlayer')
+.addEventListener('click',() =>{
+  window.location.href = 'game-start.html'
+} )
+
+
+
+
+
+
+
+
+
+
+// const gameMenu = document.getElementById('NewGameMenu')
+// console.log(gameMenu)
+// const player1 = document.getElementsByClassName('player-X')
+// const player2 = document.getElementsByClassName('player-O')
+// const  cpubtn =  document.getElementsByClassName('NewGame-VS-CPU')
+// const playerbtn = document.getElementsByClassName('NewGame-VS-PLAYER')
+// console.log(playerbtn)
+// const PlayOptions = document.getElementById("Play-Option");
+
+const sceneNewGame = document.getElementById("NewGameMenu");
+const playerX = document.getElementsByClassName('player-X')
+const playerO = document.getElementsByClassName('player-O')
+const cpubtn = document.getElementById("NewGame-VS-CPU");
+const multibtn = document.getElementById("NewGame-VS-PLAYER");
+
+let selectedPlayer = document.querySelector ('.playerX.active, .player-O"')
+
+let currentTurn = "player-x";
+sceneGameboard.setAttribute("data-turn", currentTurn)
+
+let fields = new Array(9).fill('');
+
+let winner = "";
+
+let mode = "";
+
+
+btnSelectX.addEventListener("click", (e) => {
+  btnSelectX.classList.add("active");
+  btnSelectX.disabled = true;
+  btnSelectO.classList.remove("active");
+  btnSelectO.disabled = false;
+  selectedPlayer = btnSelectX.id;
+});
+btnSelectO.addEventListener("click", (e) => {
+  btnSelectX.classList.remove("active");
+  btnSelectX.disabled = false;
+  btnSelectO.classList.add("active");
+  btnSelectO.disabled = true;
+  selectedPlayer = btnSelectO.id;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
