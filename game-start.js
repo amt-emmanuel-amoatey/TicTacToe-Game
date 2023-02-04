b5d7b9a952d226e7c32d36ff5b259636991f1da2
Amoatey <emmanuel.amoatey@amalitech.org>
2023-02-04 12:43:17 +0000

    inserted player X image

--- game-start.js
+++ game-start.js
@@ -3,6 +3,24 @@ const cellBoard = document.getElementsByClassName('cellBoard')
 const headDisplay = document.getElementsByClassName('headDisplay')
 const playerTurnDisplay = document.getElementsByClassName('player-turn')
 const cellContainer = document.getElementById ('cellContainer')
+let cellElements = document.querySelectorAll('.cell-index')
+
+// click each cell once
+cellElements.forEach(cell => {
+  
+  cell.addEventListener('click',  handleClick, {once: true})
+ 
+})
+
+function handleClick(e){
+  console.log('clicked') 
+}
+
+
+
+
+
+
 
 // restart modal Display
 const modal = document.querySelector('.modal')
@@ -35,29 +53,61 @@ let yesRestartBtn= document
 
 
 
-
-
-
-
-
-
-
- 
-
 // turn indicator
-function hitCell(cell) {
-  let boxChoice = document.getElementById(cell)
-   img.src = document.createElement('img')
+// function hitCell(cell) {
+//   let boxChoice = document.getElementById(cell)
+//    img.src = document.createElement('img')
 
-   if (player-turn.getAttribute('data-value') === "X") {
-    img.src = './assets/icon-x.svg';
-    img.setAttribute('class' , 'cellPlayed');
-    cellChoice.appendChild(img);
-    cellChoice.classList.remove('hoverClassX');
-    cellChoice.setAttribute('data-value' , 'X');
-    cellChoice.setAttribute('onclick', '' )
-    turn.setAttribute('data-value', 'O')
-    turn.src = './assets/icon-o-turn.svg';
-   }
-}
+//    if (player-turn.getAttribute('data-value') === "X") {
+//     img.src = './assets/icon-x.svg';
+//     img.setAttribute('class' , 'cellPlayed');
+//     cellChoice.appendChild(img);
+//     cellChoice.classList.remove('hoverClassX');
+//     cellChoice.setAttribute('data-value' , 'X');
+//     cellChoice.setAttribute('onclick', '' )
+//     turn.setAttribute('data-value', 'O')
+//     turn.src = './assets/icon-o-turn.svg';
+//    }
+// }
+
+
+
+
+//  let imgX = document.createElement("img");
+//  imgX.src = "./assets/icon-x.svg";
+//  console.log(imgX);
+let cell_index = document.querySelectorAll(".cell-index");
+  let imgX = document.createElement('img')
+  imgX.src = './assets/icon-x.svg';
+
+cell_index.forEach((item) => {
+  item.addEventListener("click" , ()=>{
+    if(item.childNodes == 1){
+      item = item
+    }else{
+    item.appendChild(imgX);
+     item.classList += " new_class";
+    }
+  console.log(item);
+
+  })
+
+});
+
+// insert player images
+// function test() {
+//   let imgX = document.createElement('img')
+//   imgX.src = './assets/icon-x.svg';
+//    for (let index = 0; index < cell_index.length ; index++) {
+//      let element = cell_index[index];
+//      element.appendChild(imgX);
+//      console.log(element);
+//    }
+//      return;
+
+// }
+
+
+
+// test();
 
--- new-game.js
+++ new-game.js
@@ -16,15 +16,52 @@ markO.addEventListener('click', function(){
 // cpu vs player button function (single player)
 let cpuBtn = document.getElementById('newGameVSCPU')
 .addEventListener('click', () => {
+  if (!markX.checked && !markO.checked) {
+    alert('SELECT PLAYER X or O')
+    return
+  }
   window.location.href = 'game-start.html'
 })
 
 // new game vs player (multi player button)
 let multiPlayerBtn = document.getElementById('newGameVSPlayer')
 .addEventListener('click',() =>{
+   if (!markX.checked && !markO.checked) {
+     alert('SELECT PLAYER X or O');
+     return;
+   }
   window.location.href = 'game-start.html'
 } )
 
+// select whose turn
+const XradioBtn = document.getElementById('X-mark')
+const OradioBtn = document.getElementById('O-mark')
+  function newGameVSCPU () {
+    if (XradioBtn.checked === true) {
+      huPlayer = 'X'
+      aiPlayer = 'O'
+      player1Name.innerHTML = 'X (CPU)'
+      player2Name.innerHTML = '0 (You)'
+    } else{
+      huPlayer = "O"
+      aiPlayer = "X"
+      player1Name.innerHTML = 'X (You)'
+      player2Name.innerHTML = 'O (CPU)'
+    }
+    const cpuBtn = document.getElementById('newGameVSCPU')
+    cpuBtn.setAttribute('data-value', "active")
+    playerBtn.setAttribute('data-value', '')
+    cpuTurn();
+  }
+
+  function NewGameVSPLAYER(){
+    player1Name.innerHTML = 'X (P1)'
+    player2Name.innerHTML = '0 (P2)'
+    const playerBtn = document.getElementById('newGameVSPlayer')
+    playerBtn.setAttribute('data-value','active')
+    cpuBtn.setAttribute('data-value', '')
+  }
+    
 
 
 
@@ -34,47 +71,42 @@ let multiPlayerBtn = document.getElementById('newGameVSPlayer')
 
 
 
-// const gameMenu = document.getElementById('NewGameMenu')
-// console.log(gameMenu)
-// const player1 = document.getElementsByClassName('player-X')
-// const player2 = document.getElementsByClassName('player-O')
-// const  cpubtn =  document.getElementsByClassName('NewGame-VS-CPU')
-// const playerbtn = document.getElementsByClassName('NewGame-VS-PLAYER')
-// console.log(playerbtn)
-// const PlayOptions = document.getElementById("Play-Option");
-
-const sceneNewGame = document.getElementById("NewGameMenu");
-const playerX = document.getElementsByClassName('player-X')
-const playerO = document.getElementsByClassName('player-O')
-const cpubtn = document.getElementById("NewGame-VS-CPU");
-const multibtn = document.getElementById("NewGame-VS-PLAYER");
-
-let selectedPlayer = document.querySelector ('.playerX.active, .player-O"')
-
-let currentTurn = "player-x";
-sceneGameboard.setAttribute("data-turn", currentTurn)
-
-let fields = new Array(9).fill('');
-
-let winner = "";
-
-let mode = "";
 
 
-btnSelectX.addEventListener("click", (e) => {
-  btnSelectX.classList.add("active");
-  btnSelectX.disabled = true;
-  btnSelectO.classList.remove("active");
-  btnSelectO.disabled = false;
-  selectedPlayer = btnSelectX.id;
-});
-btnSelectO.addEventListener("click", (e) => {
-  btnSelectX.classList.remove("active");
-  btnSelectX.disabled = false;
-  btnSelectO.classList.add("active");
-  btnSelectO.disabled = true;
-  selectedPlayer = btnSelectO.id;
-});
+
+
+// const sceneNewGame = document.getElementById("NewGameMenu");
+// const playerX = document.getElementsByClassName('player-X')
+// const playerO = document.getElementsByClassName('player-O')
+// const cpubtn = document.getElementById("NewGame-VS-CPU");
+// const multibtn = document.getElementById("NewGame-VS-PLAYER");
+
+// let selectedPlayer = document.querySelector ('.playerX.active, .player-O"')
+
+// let currentTurn = "player-x";
+// sceneGameboard.setAttribute("data-turn", currentTurn)
+
+// let fields = new Array(9).fill('');
+
+// let winner = "";
+
+// let mode = "";
+
+
+// btnSelectX.addEventListener("click", (e) => {
+//   btnSelectX.classList.add("active");
+//   btnSelectX.disabled = true;
+//   btnSelectO.classList.remove("active");
+//   btnSelectO.disabled = false;
+//   selectedPlayer = btnSelectX.id;
+// });
+// btnSelectO.addEventListener("click", (e) => {
+//   btnSelectX.classList.remove("active");
+//   btnSelectX.disabled = false;
+//   btnSelectO.classList.add("active");
+//   btnSelectO.disabled = true;
+//   selectedPlayer = btnSelectO.id;
+// });
 
 
 
